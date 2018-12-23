3618ec9177f4d3bd71b96c2e6e5241a3f59b07cc
Alexey Khruhsch <alexeykhr@outlook.com>
2018-12-23 22:21:37 +0200

    Initial - default route, .env, 2 libraries and basic html (web)

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	/*
+	 * Import Environment
+	 */
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Set mode: release or debug
+	gin.SetMode(os.Getenv("GIN_MODE"))
+
+	/*
+	 * Route
+	 */
+	route := gin.Default()
+
+	// Share static files
+	route.Static("/web", "./web")
+	route.LoadHTMLGlob("./web/index.html")
+
+	route.GET("/web", func(c *gin.Context) {
+		if pusher := c.Writer.Pusher(); pusher != nil {
+			// use pusher.Push() to do server push
+			if err := pusher.Push("/web/main.js", nil); err != nil {
+				log.Printf("Failed to push: %v", err)
+			}
+		}
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+
+	// Start Server
+	err = route.RunTLS(":8080", os.Getenv("CERT_FILE_PATH"), os.Getenv("CERT_KEY_PATH"))
+	if err != nil {
+		log.Fatal("Error RunTLS server")
+	}
+}
