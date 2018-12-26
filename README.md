780fb956083a3db5ebd2d2232b3bca09f6adac73
Alexey Khruhsch <alexeykhr@outlook.com>
2018-12-26 21:30:48 +0200

    Install gorm + sqlite3

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # server
+
+## Errors
+- `exec: "gcc": executable file not found in %PATH%` -
+[github.com/mattn/go-sqlite3](https://github.com/mattn/go-sqlite3/issues/435#issuecomment-314247676)
--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"os"
 
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"github.com/joho/godotenv"
 	"github.com/kataras/iris"
-
 	"github.com/kataras/iris/middleware/logger"
 	"github.com/kataras/iris/middleware/recover"
 )
@@ -19,6 +20,13 @@ func main() {
 		panic("env not loaded")
 	}
 
+	/* - Connect to Database - */
+	db, err := gorm.Open("sqlite3", os.Getenv("DATABASE_PATH"))
+	if err != nil {
+		panic("failed to connect database")
+	}
+	defer db.Close()
+
 	/* - Initialization Iris - */
 	app := iris.New()
 	app.Logger().SetLevel(os.Getenv("LOGGER"))
@@ -29,9 +37,8 @@ func main() {
 	app.Use(recover.New())
 	app.Use(logger.New())
 
+	// SPA (git submodule, dist folder)
 	app.StaticWeb("/", PathWeb)
-
-
 
 	app.Run(iris.Addr(":8080"), iris.WithoutServerError(iris.ErrServerClosed))
 }
