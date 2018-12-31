2d9f2ca43bb7f5fc1e9b063149136c46452bd34d
Alexey Khruhsch <alexeykhr@outlook.com>
2018-12-31 12:55:19 +0200

    Add badget session. Basic beforeRoute

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"./models"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
@@ -11,15 +12,25 @@ import (
 	"github.com/kataras/iris/context"
 	"github.com/kataras/iris/middleware/logger"
 	"github.com/kataras/iris/middleware/recover"
+	"github.com/kataras/iris/sessions"
+	"github.com/kataras/iris/sessions/sessiondb/badger"
 )
-
-var db *gorm.DB
 
 const PathWeb = "./web/dist/"
 
+var (
+	db      *gorm.DB
+	session *sessions.Sessions
+
+	publicRouteName = []string{
+		"GET/*file",
+	}
+)
+
 func main() {
-	db, app := Init()
+	db, session, app := Init()
 	defer db.Close()
+	defer session.Close()
 
 	// SPA (git submodule, dist folder)
 	app.StaticWeb("/", PathWeb)
@@ -29,23 +40,29 @@ func main() {
 	app.Run(iris.Addr(":8080"))
 }
 
+func beforeRoute(ctx iris.Context) {
+	sess := session.Start(ctx)
+	isAuth, _ := sess.GetBoolean("isAuth")
+	routeName := ctx.GetCurrentRoute().Name()
+
+	if !isAuth && strings.Index(strings.Join(publicRouteName, ","), routeName) == -1 {
+		ctx.StatusCode(iris.StatusUnauthorized)
+		ctx.Text("Error")
+		return
+	}
+
+	ctx.Next()
+}
+
 func routeApi(app *iris.Application) {
 	api := app.Party("/api")
 
 	api.Get("/users", func(ctx context.Context) {
 		ctx.Text("Success")
 	})
-
-	// FIXME Example
-	//api.Get("/api/users", func(ctx context.Context) {
-	//	var user models.User
-	//	db.First(&user, 1)
-	//
-	//	ctx.JSON(user)
-	//})
 }
 
-func Init() (*gorm.DB, *iris.Application) {
+func Init() (*gorm.DB, *badger.Database, *iris.Application) {
 	/* - Import Environment - */
 	err := godotenv.Load()
 	if err != nil {
@@ -58,6 +75,26 @@ func Init() (*gorm.DB, *iris.Application) {
 	if err != nil {
 		panic(err)
 	}
+
+	/* - Sessions - */
+	sessionDatabase, err := badger.New("./data")
+	if err != nil {
+		panic(err)
+	}
+
+	// Close and unlock the database when control+C/cmd+C pressed
+	iris.RegisterOnInterrupt(func() {
+		db.Close()
+		sessionDatabase.Close()
+	})
+
+	session = sessions.New(sessions.Config{
+		Cookie:       "_session",
+		Expires:      30 * 24 * time.Hour, // <= 0 means unlimited life
+		AllowReclaim: true,
+	})
+
+	session.UseDatabase(sessionDatabase)
 
 	/* - Initialization Iris - */
 	app := iris.New()
@@ -73,19 +110,5 @@ func Init() (*gorm.DB, *iris.Application) {
 	// are applied to existing routes and future routes.
 	app.UseGlobal(beforeRoute)
 
-	return db, app
-}
-
-func beforeRoute(ctx iris.Context) {
-	var user models.User
-	db.First(&user, 1)
-	println(user.ID)
-
-	if user.ID < 1 {
-		return
-	}
-
-	ctx.Values().Set("user", user)
-	println("Before")
-	ctx.Next()
+	return db, sessionDatabase, app
 }
